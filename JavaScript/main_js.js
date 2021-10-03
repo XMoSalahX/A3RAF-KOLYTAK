af9a95e0d6d914922c52ee0f06692340abf5eb23
XMoSalahX <01154615199>
2021-10-03 19:57:28 +0200

    Up To Top Speed

--- JavaScript/main_js.js
+++ JavaScript/main_js.js
@@ -1,3 +1,27 @@
+const loadFont = (url) => {
+    // the 'fetch' equivalent has caching issues
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.onreadystatechange = () => {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            let css = xhr.responseText;
+            css = css.replace(/}/g, 'font-display: swap; }');
+
+            const head = document.getElementsByTagName('head')[0];
+            const style = document.createElement('style');
+            style.appendChild(document.createTextNode(css));
+            head.appendChild(style);
+        }
+    };
+    xhr.send();
+}
+
+loadFont('https://fonts.googleapis.com/css2?family=Cairo:wght@200;300;400;600;700;900&display=swap');
+
+
+
+
+
 const test = document.querySelector('h1')
 const Button1 = document.querySelector('.button1')
 const Button2 = document.querySelector('.button2')
